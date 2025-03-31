4e1f3d6ea0cc3b012194aa1fe37b918e144df2bb
leonsrk <141861431+leonsrk@users.noreply.github.com>
2025-03-31 17:13:21 +0200

    updated some classes

--- classes.py
+++ classes.py
@@ -1,21 +1,22 @@
 class player():
 
-    def __init__(self, id: str, score: int, win: bool) -> None:
+    def __init__(self, id: str) -> None:
         self.id = id
-        self.score = score
+        self.score = 0
+        self.move = False
         self.win = False
 
 
 class field():
 
-    def __init__(self, id: str, pos: tuple[int, int]) -> None:
+    def __init__(self, pos: tuple[int, int]) -> None:
         self.id = "f"
         self.pos = pos
 
 
 class game():
 
-    def __init__(self, row1: list[str], row2: list[str], row3: list[str]) -> None:
+    def __init__(self, row1: list[field], row2: list[field], row3: list[field]) -> None:
         self.row1 = row1
         self.row2 = row2
         self.row3 = row3
