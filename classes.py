58bd11523765936757cda8e18d2e58d9e6f2dd69
leonsrk <141861431+leonsrk@users.noreply.github.com>
2025-03-31 16:44:22 +0200

    created game class

--- classes.py
+++ classes.py
@@ -8,6 +8,14 @@ class player():
 
 class field():
 
-    def __init__(self, id: str, pos: (int)) -> None:
+    def __init__(self, id: str, pos: tuple[int, int]) -> None:
         self.id = "f"
         self.pos = pos
+
+
+class game():
+
+    def __init__(self, row1: list[str], row2: list[str], row3: list[str]) -> None:
+        self.row1 = row1
+        self.row2 = row2
+        self.row3 = row3
