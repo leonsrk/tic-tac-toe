df4fd17d009634365dd191c1e001692acdf68e2f
leonsrk <141861431+leonsrk@users.noreply.github.com>
2025-04-18 17:09:15 +0200

    added cell class

--- classes.py
+++ classes.py
@@ -2,21 +2,13 @@ class player():
 
     def __init__(self, id: str) -> None:
         self.id = id
-        self.score = 0
         self.move = False
         self.win = False
 
 
-class field():
+class cell():
 
-    def __init__(self, pos: tuple[int, int]) -> None:
-        self.id = "f"
+    def __init__(self, pos, taken: str, num: int) -> None:    # taken = f: free; p: player; e: enemy
         self.pos = pos
-
-
-class game():
-
-    def __init__(self, row1: list[field], row2: list[field], row3: list[field]) -> None:
-        self.row1 = row1
-        self.row2 = row2
-        self.row3 = row3
+        self.taken = taken
+        self.num = num
