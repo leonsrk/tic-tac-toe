b2766c1c846b4391dcea00a5ca351ff84c3b1723
leonsrk <141861431+leonsrk@users.noreply.github.com>
2025-05-14 18:01:08 +0200

    made function to check for possible moves

--- logic.py
+++ logic.py
@@ -1,29 +1,9 @@
-def bestMove(cells: list) -> int:
-    for i, cell in enumerate(cells, start=1):
-        if cell.taken == "p":
-            if i == 1 or i == 4 or i == 7:
-                if cells[i + 1].taken == "f":
-                    return cells[i + 1].num
-                if i != 7 and cells[i + 3].taken == "f":
-                    return cells[i + 3].num
-                if i == 7 and cells[i - 3].taken == "f":
-                    return cells[i - 3].num
-            elif i == 2 or i == 5 or i == 8:
-                if cells[i + 1].taken == "f":
-                    return cells[i + 1].num
-                if cells[i - 1].taken == "f":
-                    return cells[i - 1].num
-            elif i == 3 or i == 6 or i == 9:
-                if i != 9 and cells[i + 3].taken == "f":
-                    return cells[i + 3].num
-                if i == 9 and cells[i - 3].taken == "f":
-                    return cells[i - 3].num
-                if cells[i - 1].taken == "f":
-                    return cells[i - 1].num
-            else:
-                pass
-        else:
-            for cell in cells:
-                if cell.taken == "f":
-                    return cell.num
-    return 0
+import classes
+
+
+def possibleMoves(allMoves: list[classes.Cell]) -> list:
+    posMoves = []
+    for i in allMoves:
+        if i.taken == "f":
+            posMoves.append(i)
+    return posMoves
