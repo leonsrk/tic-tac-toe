8e1db3b195698b8a83b3da824061841c3adbcb6d
leonsrk <141861431+leonsrk@users.noreply.github.com>
2025-06-03 21:33:19 +0200

    started new functions to check if a player has won the game or if the enemy can win the game with his next move

--- logic.py
+++ logic.py
@@ -1,9 +1,38 @@
 import classes
 
 
-def possibleMoves(allMoves: list[classes.Cell]) -> list:
+def win(ownCells: list[classes.Cell]) -> bool:
+    combinations = [[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9],
+                    [1, 4, 7],
+                    [2, 5, 8],
+                    [3, 6, 9],
+                    [3, 5, 7],
+                    [1, 5, 9]
+                    ]
+    for combination in combinations:
+        for cell in combination:
+            pass  # TODO
+
+
+# -------------------- enemy logic -----------------------
+
+enemyCells: list[classes.Cell] = []
+
+
+def possibleMoves(allMoves: list[classes.Cell]):
+    """returns a list with all the possible moves for the next turn
+    Args:
+        allMoves (list): list with all moves, contains objects of Cell
+    """
     posMoves = []
     for i in allMoves:
         if i.taken == "f":
             posMoves.append(i)
-    return posMoves
+    print("posMoves:", posMoves)  # -------------------------------DEBUG
+    possibleWin(posMoves, enemyCells)
+
+
+def possibleWin(posMoves: list[classes.Cell], ownCells: list[classes.Cell]):
+    pass  # TODO
